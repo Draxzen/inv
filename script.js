79d6b7ab9e7dfde93f3b65301b11a3e89e4de286
Draxzen <160305593+Draxzen@users.noreply.github.com>
2024-02-17 17:11:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('confirmBtn').addEventListener('click', function() {
+    alert('¡Genial Marce! ¡vamos a por ese molten👊!');
+    // Aquí podrías agregar más código para enviar la invitación por correo electrónico o cualquier otro método.
+});
+document.getElementById('denegar').addEventListener('click', function() {
+    alert('Bueno, será para otra ocasíon :)');
+})
